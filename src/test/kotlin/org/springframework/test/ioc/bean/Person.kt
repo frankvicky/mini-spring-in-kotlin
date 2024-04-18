d2a9e5c99f83b823d166e2ed6b4a49fc8cdc6d93
frankvicky <chi972121@gmail.com>
2024-04-18 23:18:09 +0800

    :recycle: 測試用 Person 類別改為 data class

--- src/test/kotlin/org/springframework/test/ioc/bean/Person.kt
+++ src/test/kotlin/org/springframework/test/ioc/bean/Person.kt
@@ -1,13 +1,3 @@
 package org.springframework.test.ioc.bean
 
-//class Person() {
-//    var name: String? = null
-//    var age: Int? = null
-//
-//    constructor(name: String, age: Int): this() {
-//        this.name = name
-//        this.age = age
-//    }
-//}
-
-class Person(var name: String? = null, var age: Int? = null)
+data class Person(var name: String? = null, var age: Int? = null)
